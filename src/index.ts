c55d944790c9fdd497e2204c5e78088bfe5ef766
Abhik <abhikgupta01@gmail.com>
2024-07-13 11:33:20 +0530

    Added restauran create, update and get APIs

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,18 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoutes from './routes/MyUserRoute';
+import myRestaurantRoutes from './routes/MyRestaurantRoute';
+import { v2 as cloudinary } from 'cloudinary';
 
 mongoose
     .connect(process.env.MONGODB_CONNECTION_URI as string)
-    .then(() => console.log(chalk.hex('#00ff00').bold(`<--- DATABASE CONNECTED SUCCESSFULLY --->`)))
+    .then(() => console.log(chalk.hex('#00ff00').bold(`<--- DATABASE CONNECTED SUCCESSFULLY --->`)));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 app.use(express.json());
@@ -18,6 +26,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoutes);
+app.use("/api/my/restaurant", myRestaurantRoutes);
 
 app.listen(7000, () => {
     console.log(chalk.hex('#00ff00').bold(`<--- SERVER RUNNING AT PORT 7000 --->`));
